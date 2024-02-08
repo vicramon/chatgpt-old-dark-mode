009d544a14386bfe510a2518747c474c76cc45aa
Vic Ramon <v@vicramon.com>
2024-02-07 23:58:39 -0800

    Initial commit

---
+++ contentScript.js
@@ -0,0 +1,22 @@
+document.documentElement.style.setProperty('--sidebar-surface-primary', '#0D0D0D');
+document.documentElement.style.setProperty('--main-surface-primary', '#34353F');
+
+// These are the other root variables. I'm not sure what else changed, I don't have a good screenshot of the old one. I just go the sidebar and main surface for now.
+
+// --text-primary: var(--gray-50);
+// --text-secondary: var(--gray-300);
+// --text-tertiary: var(--gray-500);
+// --text-quaternary: var(--gray-700);
+// --border-light: rgba(217,217,227,.1);
+// --border-medium: rgba(217,217,227,.15);
+// --border-heavy: rgba(217,217,227,.2);
+// --border-xheavy: rgba(217,217,227,.25);
+// --main-surface-primary: var(--gray-900);
+// --main-surface-secondary: var(--gray-850);
+// --main-surface-tertiary: var(--gray-800);
+// --sidebar-surface-primary: var(--gray-950);
+// --sidebar-surface-secondary: var(--gray-850);
+// --sidebar-surface-tertiary: var(--gray-800);
+// --popover-surface-primary: var(--gray-900);
+// --link: #7ab7ff;
+// --link-hover: #5e83b3;
